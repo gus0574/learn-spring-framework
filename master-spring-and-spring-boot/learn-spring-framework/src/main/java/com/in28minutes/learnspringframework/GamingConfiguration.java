e75d143dccfdbb158ace014f3c2f737505eeface
gus0574 <gus0574@gmail.com>
2023-09-05 18:48:51 +0900

    learning spring

---
+++ master-spring-and-spring-boot/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/GamingConfiguration.java
@@ -0,0 +1,24 @@
+package com.in28minutes.learnspringframework;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.GamingConsole;
+import com.in28minutes.learnspringframework.game.PacmanGame;
+
+@Configuration
+public class GamingConfiguration {
+	
+	@Bean
+	public GamingConsole game() {
+		var game = new PacmanGame();
+		return game;
+	}
+	
+	@Bean
+	public GameRunner gameRunner(GamingConsole game) {	// 매개변수로 스프링Bean을 사용할 수 있다 
+		GameRunner gameRunner = new GameRunner(game);
+		return gameRunner;
+	}
+}
